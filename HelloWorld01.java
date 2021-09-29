3af5ed49ca19735af494746ee194a63d52ce18d1
praphullakherade <91040720+praphullakherade@users.noreply.github.com>
2021-09-29 13:40:50 +0530

    Added Hello India

--- HelloWorld01.java
+++ HelloWorld01.java
@@ -3,6 +3,6 @@ package com.company;
 public class HelloWorld01 {
     public static void main(String[] args) {
 
-        System.out.println("Hello Remote");
+        System.out.println("Hello India");
     }
 }
