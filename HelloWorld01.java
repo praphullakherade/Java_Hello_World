3736dd472d9b5ab0df800c609cda56e2d8857409
praphullakherade <91040720+praphullakherade@users.noreply.github.com>
2021-09-29 13:31:21 +0530

    making directly changes on github

--- HelloWorld01.java
+++ HelloWorld01.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class HelloWorld01 {
     public static void main(String[] args) {
-        System.out.println("Hello World 01");
+        System.out.println("Hello Local Machine");
     }
 }
